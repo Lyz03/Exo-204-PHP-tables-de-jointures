e6df05d54736388739367974b398719494b2ccbd
Lyz03 <lizoe@netcourrier.com>
2022-02-25 13:25:48 +0100

    exo 204

--- index.php
+++ index.php
@@ -7,3 +7,20 @@
  * 4 - A l'aide d'un simple print_r, afficher les rôles de chaque utilisateur.
  * 5 - FIN !
  */
+
+$db = new PDO('mysql:host=localhost;dbname=exo_204;charset=utf8', 'root', '');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$stmt = $db->prepare("
+    SELECT user.username, role.role
+    FROM user_role
+    INNER JOIN user ON user_role.user_id = user.id
+    INNER JOIN role ON user_role.role_id = role.id
+");
+
+if ($stmt->execute()) {
+    echo '<pre>';
+    print_r($stmt->fetchAll());
+    echo '</pre>';
+}
